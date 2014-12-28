e21967b67246403286976c0fc95492769cad3267
Nazarii Gudzovatyi <nazarii@enapps.co.uk>
2014-12-28 20:16:37 +0200

    Added loading city posts for NYT; Added config file to not store secret keys.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function loadData() {
 
     var $body = $('body');
@@ -6,21 +5,30 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
-    var city = $('#city').val();
-    var street = $('#street').val();
-    var address = city + ', ' + street;
-    var streetview_url = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
     // load streetview
-    $greeting.text('So, you do want to live in ' + address);
+    var city = $('#city').val();
+    var street = $('#street').val();
+    var address = city + ', ' + street;
+    var streetview_url = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
     var image = "<img class='bgimg' src='" + streetview_url + "'/>";
+    $greeting.text('So, you do want to live in ' + address);
     $body.append(image);
 
-    // YOUR CODE GOES HERE!
+    // NY Times articles
+    var nyt_api_key = config['nyt_api_key'];
+    var search_url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=glocations:("' + city + '")&api-key=' + nyt_api_key
+    $.getJSON( search_url, function( data ) {
+        var response = data.response;
+        for (var i=0; i<response.docs.length; i++) {
+            var doc = response.docs[i];
+            $nytElem.append('<li class="article"><a href="' + doc.web_url + '">' + doc.snippet + '</a></li>')
+}
+    });
 
     return false;
 };
