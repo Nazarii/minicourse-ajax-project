a405b621c286cd7e85cf5783678050e5adad2fe7
Nazarii Gudzovatyi <nazarii@enapps.co.uk>
2014-12-28 18:35:52 +0200

    MOD: added Street View background image set on form submission

--- js/script.js
+++ js/script.js
@@ -6,12 +6,18 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
+    var city = $('#city').val();
+    var street = $('#street').val();
+    var address = city + ',%20' + street;
+    var streetview_url = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
     // load streetview
+    $greeting.text('So you do want to live in ' + city + ', ' + street);
+    $body.css('background-image', 'url(' + streetview_url + ')');
 
     // YOUR CODE GOES HERE!
 
